557995216e72e56289a614275e39a05b5750b6a1
Lipheid <ryudong12@naver.com>
2024-02-28 16:20:46 +0900

    main

--- shells/useradd_file.sh
+++ shells/useradd_file.sh
@@ -5,5 +5,5 @@ input="user.dat"
 while IFS=`,` read -r username uid gid comment
 do
     echo "Adding $username"
-    useradd -u "$uid" -g "$gid -c "$comment" -m "$username"
+    useradd -u "$uid" -g "$gid -c "$cosmment" -m "$username"
 done < $input
