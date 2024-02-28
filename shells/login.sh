7e6104d84c81dd9ad5858c43d7d876dc54e6240b
Lipheid <ryudong12@naver.com>
2024-02-28 15:47:39 +0900

    main

--- shells/login.sh
+++ shells/login.sh
@@ -2,11 +2,11 @@
 
 opt1=$1
 opt2=$2
-s
+
 if [ $# -eq 2 ]; then
     if [ $opt1 == 'test' -a $opt2 == 'aaa' ]; then
         echo good
-    if [ $opt1 == 'aaa' -a $opt2 == 'test' ]; then
+    elif [ $opt1 == 'aaa' -a $opt2 == 'test' ]; then
         echo good
     else
         echo bad
