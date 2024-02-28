aa7784928283ef4f404fdf2fd36f19512b049853
Lipheid <ryudong12@naver.com>
2024-02-28 15:00:00 +0900

    main

--- shells/while_new.sh
+++ shells/while_new.sh
@@ -8,8 +8,7 @@ while [ $a != "0" ]; do
 
     if [ $a != "0" ]; then
         for ((k=1;k<=9;k++)); do
-        do
-            echo "$a *$k = `expr $a \* $k`"
+            echo " $a *$k = `expr $a \* $k `"
         done
     fi
 done
