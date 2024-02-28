d74f88d669412c72f0bf171f49f2bfc8891e53e5
Lipheid <ryudong12@naver.com>
2024-02-28 15:12:09 +0900

    main

--- shells/while2.sh
+++ shells/while2.sh
@@ -2,13 +2,15 @@
 
 a=1
 
-while [ $a != "0" ]; do
+while [ $a != "0" ] ; do
     echo -n "input : "
     read a
 
-    if [ $a -eq 0 ]; then
+    if [ $a -eq 0 ] ; then
     echo "The number of inputs must be between 2 and 9"
-    elif [ $a -gt 9]; then
+    elif [ $a -eq 1] ; then
+    echo "The number of inputs must be between 2 and 9"
+    elif [ $a -gt 9] s; then
     echo "The number of inputs must be between 2 and 9"
     else
         for ((k=1;k<=9;k++)); do
