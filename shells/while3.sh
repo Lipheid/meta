f940280f83f4d04f39fb1e49ba5542ac2b870083
Lipheid <ryudong12@naver.com>
2024-02-28 15:14:22 +0900

    main

--- shells/while3.sh
+++ shells/while3.sh
@@ -8,9 +8,7 @@ while [ $a != "0" ]; do
 
     if [ $a -eq 0 ]; then
     break
-    elif [ $a -eq 1 ]; then
-    echo "The number of inputs must be between 2 and 9"
-    elif [ $a -gt 9 ]; then
+    elif [ $a -lt 0 -o $a -eq 1 -o $a -gt 9 ]; then
     echo "The number of inputs must be between 2 and 9"
     else
         for ((k=1;k<=9;k++)); do
