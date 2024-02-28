d75879b274742d73704c1560e94ee74c1e7da935
Lipheid <ryudong12@naver.com>
2024-02-28 14:57:27 +0900

    main

--- shells/while.sh
+++ shells/while.sh
@@ -6,7 +6,7 @@ while [ $a != "0" ]; do
     echo -n "input : "
     read a
 
-    if [ $a != "0"]; then
+    if [ $a != "0" ]; then
         for k in 1 2 3 4 5 6 7 8 9
         do
             echo "$a *$k = `expr $a \* $k`"
