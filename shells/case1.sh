5c98a0bd401d3730219285ddc229c8862953f1c5
Lipheid <ryudong12@naver.com>
2024-02-28 14:25:31 +0900

    main

---
+++ shells/case1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo -n "input: "
+read a
+
+case $((a / 10)) in
+    9)
+        echo A
+        ;;
+    8)
+        echo B
+        ;;
+    7)
+        echo C
+        ;;
+    6)
+        echo D
+        ;;
+    [0-5])
+        echo F
+        ;;
+    *)
+        echo "input the score"
+        ;;
+esac
+
+echo "Thank you~ Bye!"
